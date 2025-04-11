5a00d445b579cef20feca3c56b5e3ee5f2c1334b
chunchunmaru0000 <cho227675m@gmail.com>
2025-04-11 14:57:16 +0300

    fix return

--- Statements/Return.cs
+++ Statements/Return.cs
@@ -7,10 +7,12 @@ public class ReturnStatement(ICompilable value) : ICompilable
     public Instruction Compile()
     {
         Instruction value = Value.Compile();
+        int argsOffset = Compiler.GetLastVarOffset();
+        argsOffset += argsOffset % 16;
         return new(value.Type, Comp.Str([
             value.Code,
             $"    pop rax",
-            $"    add rsp, {Compiler.GetLastVarOffset()} ; ОЧИЩЕНИЕ СТЕКА ОТ ПАРАМЕТРОВ",
+            $"    add rsp, {argsOffset} ; ОЧИЩЕНИЕ СТЕКА ОТ ПАРАМЕТРОВ",
             $"    pop rbp ; ВЫКРИВДЕНИЕ СТЭКА НАЗАД ЧТОБЫ ПОТОМ ОН БЫЛ ВОССТАНОВЛЕН САМ ЧЕРЕЗ pop rip ПРИ ret",
             $"    ret ; ВОЗВРАЩАЕТ ЗНАЧЕНИЕ ТИПА {value.Type.Log()}"
         ]));
