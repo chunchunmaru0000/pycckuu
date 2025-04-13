d323da6e68ec5b0f082c525bd6e29d4eaafbcdfe
chunchunmaru0000 <cho227675m@gmail.com>
2025-04-13 21:33:03 +0300

    args

--- Program.cs
+++ Program.cs
@@ -2,6 +2,17 @@
 
 class Program
 {
+	private static string ExitEnter(string msg, ConsoleColor color = ConsoleColor.Red)
+	{
+		Console.ForegroundColor = color;
+		Console.WriteLine(msg);
+		Console.ResetColor();
+		Console.WriteLine("[Enter] чтобы выйти");
+		Console.ReadLine();
+		Environment.Exit(0);
+		return "";
+	}
+#if DEBUG
 	private static string AsmName = "some.asm";
 	private static string AsmPath = $"V:\\langs\\as\\test\\{AsmName}";
 	private static string IncPath = "V:\\fasm\\INCLUDE\\";
@@ -20,4 +31,33 @@ class Program
 
 		Console.ReadLine();
 	}
+#else
+	static void Main(string[] args)	
+	{
+		if (args.Length < 1)
+			ExitEnter($"НЕДОСТАТОЧНО АГРУМЕНТОВ, БЫЛО ВСЕГО[{args.Length}]");
+        string path = args[0];
+
+		if (!File.Exists(path))
+			ExitEnter($"ФАЙЛ НЕ СУЩЕСТВУЕТ [{path}]");
+
+		string incPath = args.FirstOrDefault(a => a.ToLower().EndsWith(".inc"), 
+			Path.Combine(Path.GetDirectoryName(path)!, "INCLUDE/WIN64A.INC"));
+		if (!File.Exists(incPath))
+			ExitEnter($"ФАЙЛ НЕ СУЩЕСТВУЕТ [{incPath}]");
+
+        string asm = args.FirstOrDefault(a => a.ToLower().EndsWith(".asm"),
+            Path.ChangeExtension(path, ".asm"));
+
+        string input = File.ReadAllText(path, System.Text.Encoding.UTF8);
+		Token[] tokens = new Tokenizator(input).Tokenize();
+		string code = new Compiler("w", incPath, tokens).Compile();
+		Console.WriteLine(code);
+
+		Console.WriteLine($"{asm}\n");
+		Console.ReadLine();
+		File.WriteAllText(asm, code);
+	}
+
+#endif
 }
